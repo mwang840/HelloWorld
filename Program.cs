27006f23b5f2c1765a1cfa8b9c44382016b589a7
Maxwell Wang <maxwang@udel.edu>
2022-05-28 08:46:55 -0400

    My name is Walter Hartwell White

--- Program.cs
+++ Program.cs
@@ -3,10 +3,8 @@ using System;
 namespace HelloWorld{
     class Program{
         static void Main(string[] args){
-            int number = 0;
-            int newNum = number + 1;
-            Console.Write("The number is initially: " + number +"\n");
-            Console.Write("Now the number after adding one is: " + newNum);
+            string name = "Walter Hartwell White", location = "308 Negra Arroyo Lane, ", name3= "Alberquerque, NM";
+            Console.WriteLine("My name is: " + name + "\nI live at " + location + name3 + "\n");
         }
     }
 }
