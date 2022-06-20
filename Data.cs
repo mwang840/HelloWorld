0559b718e42fdfb7456ad342ae630c0707341818
Maxwell Wang <maxwang@udel.edu>
2022-06-20 10:37:52 -0400

    decimal/floating

--- Data.cs
+++ Data.cs
@@ -3,11 +3,11 @@ using System;
 namespace HelloWorld{
     public class Data{
         public static void Main1(string[] args){
-            Console.WriteLine("Signed Integer Bytes");
-            Console.WriteLine($"sbyte: {sbyte.MinValue} to {sbyte.MaxValue}");
-            Console.WriteLine($"short: {short.MinValue} to {short.MaxValue}");
-            Console.WriteLine($"int: {int.MinValue} to {int.MaxValue}");
-            Console.WriteLine($"long: {long.MinValue} to {long.MaxValue}");
+            Console.WriteLine("");
+            Console.WriteLine("Floating point types:");
+            Console.WriteLine($"float  : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
+            Console.WriteLine($"double : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
+            Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision)");
         }
     }
 }
